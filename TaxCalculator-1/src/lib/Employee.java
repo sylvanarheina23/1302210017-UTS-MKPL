82e1e61d40807a45d92412d03e104ac4df641e16
sylvanarheina23 <sylvana.freelance@gmail.com>
2024-04-23 15:09:26 +0700

    public class employee-employee
    Ada beberapa variabel yang tidak jelas seperti isForeigner, gender, spouseIdNumber.equals(""), dan childIdNumbers.size(). Penamaan yang lebih jelas dapat membantu memahami maksud variabel tersebut.

--- TaxCalculator-1/src/lib/Employee.java
+++ TaxCalculator-1/src/lib/Employee.java
@@ -19,33 +19,25 @@ public class Employee {
 	private int monthWorkingInYear;
 	
 	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+    private boolean gender; // true = Laki-laki, false = Perempuan
+    
+    private String spouseIdNumber;
+    private List<String> childIdNumbers;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+        // Implementasi konstruktor...
+    }
+
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+        this.spouseName = spouseName;
+        this.spouseIdNumber = spouseIdNumber;
+    }
+
+    public void addChild(String childName, String childIdNumber) {
+        childNames.add(childName);
+        childIdNumbers.add(childIdNumber);
+    }
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
 	
 	/**
 	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
